d3b3199f0ba619b3df4ced7ebed6fa3e02cfdcd6
afcastano <afcastano@afcastano-rubydev.(none)>
2014-05-04 22:23:49 +1000

    hashtag matching

--- markdown-todo.js
+++ markdown-todo.js
@@ -1,27 +1,36 @@
 markdownTodo = (function(){
 	var parser = {};
+
+	var hashtag = { 
+		expression: /(#[^<^\s]*)/g,
+		replaceWith: '<span class="hashtag">$&</span>'
+	};
 	
 	var transformations = [
 		{
 			expression: /(^\s*\-)(.*)+$/gm,
-			replaceWith: "$1 <input type='checkbox' checked>$2</input>"
+			replaceWith: '$1 <input type="checkbox" checked class="donetask">$2</input>'
+
 		},
 	
 		{
 			expression: /(^\s*\+)(.*)+$/gm,
-			replaceWith: "$1 <input type='checkbox'>$2</input>"
+			replaceWith: '$1 <input type="checkbox" class="pendingtask">$2</input>'
 		}
 	]
 
 	
 	var converter = new Markdown.Converter();
 	
-	converter.hooks.chain("preConversion", function (text) {
+	converter.hooks.chain('preConversion', function (text) {
 
 		for(var i = 0; i < transformations.length; i++){
 			var transformation = transformations[i]; 
 			text = text.replace(transformation.expression, transformation.replaceWith);
 		}
+
+		//replace hashtags
+		text = text.replace(hashtag.expression, hashtag.replaceWith );
 
 	    return text;
    	});
